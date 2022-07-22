3fec95b93c2b7582230ede70b606ef8f1e810f4d
leonardo-benetti <leonardo.benetti@usp.br>
2022-07-22 19:50:25 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace leonardo_benetti_d3_avaliacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
