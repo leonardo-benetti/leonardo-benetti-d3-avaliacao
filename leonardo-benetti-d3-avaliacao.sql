20bcba815940f4d28d6462bbbf3d37754f600d41
leonardo-benetti <leonardo.benetti@usp.br>
2022-07-22 23:02:56 -0300

    fix: update sql file to do pwd hashing

--- leonardo-benetti-d3-avaliacao.sql
+++ leonardo-benetti-d3-avaliacao.sql
@@ -3,13 +3,15 @@ CREATE DATABASE [leonardo-benetti-d3-avaliacao];
 USE [leonardo-benetti-d3-avaliacao];
 
 CREATE TABLE Users (
-	IdUser		INT IDENTITY(1,1) PRIMARY KEY,
+	IdUser		INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[Name]		VARCHAR(255) NOT NULL,
 	[Email]		VARCHAR(255) NOT NULL,
-	[Password]	VARCHAR(255) NOT NULL
+	[Password]	BINARY(64) NOT NULL
 );
 
 SELECT * FROM Users;
 
 INSERT INTO Users ([Name], Email, [Password])
-VALUES ('admin', 'admin@email.com', 'admin123');
+VALUES ('admin', 'admin@email.com', HASHBYTES('SHA2_512', 'admin123'));
+
+SELECT IdUser, Name, Email, Password FROM Users WHERE Email LIKE 'admin@email.com';
